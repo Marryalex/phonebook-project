33b77c6e9877421296b0bfe1406e3edb1e97192a
Marry <masha432alex@i.ua>
2022-12-29 16:48:07 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+
+
 
 import { fetchContacts } from 'redux/operations';
 import ContactForm from './ContactForm/ContactForm'
@@ -7,20 +11,44 @@ import ContactList from './ContactList/ContactList'
 import Filter from './Filter/Filter'
 
 
-export default function App() {
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+export default function App() {
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
   return (
-    <div className='wrapper'>
-        <h1 className='title'>Phonebook</h1>
-        <ContactForm />
-        <h2 className='title'>Contacts</h2>
-        <Filter />
-        <ContactList />
-      </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/tasks"
+          element={
+            <PrivateRoute redirectTo="/contacts" component={<ContactsPage />} />
+          }
+        />
+      </Route>
+    </Routes>
+    // {/* <div className='wrapper'>
+    //     <h1 className='title'>Phonebook</h1>
+    //     <ContactForm />
+    //     <h2 className='title'>Contacts</h2>
+    //     <Filter />
+    //     <ContactList />
+    //   </div> */}
   )
 }
 
---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,14 @@
+import { Navigation } from '../Navigation/Navigation';
+// import { UserMenu } from '../UserMenu/UserMenu';
+// import { AuthNav } from '../AuthNav/AuthNav';
+// import { useAuth } from 'hooks';
+
+export const AppBar = () => {
+
+  return (
+    <header >
+      <Navigation />
+      {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
+    </header>
+  );
+};
