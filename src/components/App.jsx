db44bc20823bbf2ab0efa812212208cd57fa5a06
Marry <masha432alex@i.ua>
2023-01-09 11:58:38 +0100

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 
+import { lazy } from 'react';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
 
 
-import { fetchContacts } from 'redux/operations';
-import ContactForm from './ContactForm/ContactForm'
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import ContactList from './ContactList/ContactList'
-import Filter from './Filter/Filter'
+// import { fetchContacts } from 'redux/operations';
+// import ContactForm from './ContactForm/ContactForm'
+// import { useDispatch } from 'react-redux';
+// import { useEffect } from 'react';
+// import ContactList from './ContactList/ContactList'
+// import Filter from './Filter/Filter'
 
-
+const HomePage = lazy(() => import('./pages/Home'));
+const RegisterPage = lazy(() => import('./pages/Register'));
+const LoginPage = lazy(() => import('./pages/Login'));
+const ContactsPage = lazy(() => import('./pages/Contacts'));
 
 export default function App() {
   // const dispatch = useDispatch();
