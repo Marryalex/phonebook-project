ad73aafd8765aa0632366ad604d1359bce95b378
Marry <masha432alex@i.ua>
2023-08-29 15:46:05 +0300

    add changes

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import { useState} from 'react'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { addContact } from 'redux/operations';
 import { getContacts } from 'redux/selectors';
@@ -18,154 +18,148 @@ import {
 
 
 
-
-
-// import styles from './ContactForm.module.css'
-// import PersonAddRoundedIcon from '@mui/icons-material/PersonAddRounded';
-
-
 export default function ContactForm() {
 
 
-const dispatch = useDispatch();
-const [name, setName] = useState('')
-const [number, setNumber] = useState('')
-const contacts = useSelector(getContacts);
-const [isSnackOpen, setIsSnackOpen] = useState(false);
+  const dispatch = useDispatch();
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+  const contacts = useSelector(getContacts);
+  const [isSnackOpen, setIsSnackOpen] = useState(false);
 
 
 
-const isContactInList = contactName => {
-  const lowercaseName = contactName.toLowerCase();
-  return contacts.find(({ name }) =>
-    name.toLowerCase().includes(lowercaseName)
-  );
-};
+  const isContactInList = contactName => {
+    const lowercaseName = contactName.toLowerCase();
+    return contacts.find(({ name }) =>
+      name.toLowerCase().includes(lowercaseName)
+    );
+  };
 
-const handleChange = (e) => {
-  const { name, value } = e.currentTarget;
-  
-  switch (name) {
-    case 'name':
+  const handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+
+    switch (name) {
+      case 'name':
         setName(value);
         break;
-    
-    case 'number':
+
+      case 'number':
         setNumber(value);
-    
+
         break;
-    
-    default:
-      throw new Error('Not supported type')
-}
-};
 
-const handleSubmit = (e) => {
-  e.preventDefault();
+      default:
+        throw new Error('Not supported type')
+    }
+  };
 
-  if (isContactInList(name)) {
-    alert(`${name} is already in contacts.`);
-    return;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isContactInList(name)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    dispatch(addContact({ name, number }));
+    setIsSnackOpen(true);
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
   }
-  dispatch(addContact({ name, number }));
-  setIsSnackOpen(true);
-  reset();
-};
-
-const reset = () => {
-  setName('');
-  setNumber('');
-}
 
 
   return (
 
 
-  <>
-    {isSnackOpen && (
-      <Snack
-        isOpen={isSnackOpen}
-        handleClose={() => {
-          setIsSnackOpen(false);
-        }}
-        text="Add Contact"
-        type="success"
-      />
-    )}
-    <Container component="section" maxWidth="xs">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <PersonAddRoundedIcon />
-        </Avatar>
-
+    <>
+      {isSnackOpen && (
+        <Snack
+          isOpen={isSnackOpen}
+          handleClose={() => {
+            setIsSnackOpen(false);
+          }}
+          text="Add Contact"
+          type="success"
+        />
+      )}
+      <Container component="section" maxWidth="xs">
         <Box
-          component="form"
-          noValidate
-          onSubmit={handleSubmit}
-          sx={{ mt: 3 }}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
         >
-          {/* <FormControl error> */}
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-    
-              <TextField
-              type="text"
-            name="name"
-            error={name === ""}
-            id='numberInputId'
-            value={name}
-            label="Name"
-            autoComplete="given-name"
-            required
-            fullWidth
-            onChange={handleChange}
-            helperText={name === "" ? 'Required' : ' '}
-    />
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <PersonAddRoundedIcon />
+          </Avatar>
+
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            {/* <FormControl error> */}
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+
+                <TextField
+                  type="text"
+                  name="name"
+                  error={name === ""}
+                  id='numberInputId'
+                  value={name}
+                  label="Name"
+                  autoComplete="given-name"
+                  required
+                  fullWidth
+                  onChange={handleChange}
+                  helperText={name === "" ? 'Required' : ' '}
+                />
+
+              </Grid>
+
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  type="text"
+                  name="number"
+                  inputProps={{
+                    inputMode: 'numeric',
+                    pattern: ''
+                  }}
+                  error={name === ""}
+                  id='numberInputId'
+                  value={number}
+                  autoComplete="tel"
+                  onChange={handleChange}
+                  required
+                  fullWidth
+                  label="Phone number"
+                  helperText={name === "" ? 'Required' : ' '}
+                />
+              </Grid>
 
             </Grid>
 
-       
-            <Grid item xs={12} sm={6}>
-              <TextField
-            type="text"
-            name="number"
-            inputProps={{ 
-           inputMode: 'numeric', 
-           pattern: ''
-    }} 
-            error={name === ""}
-            id='numberInputId'
-            value={number}
-            autoComplete="tel"
-            onChange={handleChange}
-            required 
-            fullWidth
-            label="Phone number"
-            helperText={name === "" ? 'Required' : ' '}
-              />
-            </Grid> 
-            
-          </Grid>
-    
-          <Button
-            type="submit"
-            disabled={ !name || !number }
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Add Contact
-          </Button>
+            <Button
+              type="submit"
+              disabled={!name || !number}
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Add Contact
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Container>
-  </>
+      </Container>
+    </>
   )
 }
 
